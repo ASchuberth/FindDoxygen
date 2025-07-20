0883063fb74deb00c07cbc52a8e79a350d52f55d
Austin Schuberth <aschuberth@unm.edu>
2025-07-20 07:44:06 -0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+namespace ds {
+/// @brief  struct 1 \f$\lambda\f$
+struct struct1 {
+    
+    /// @brief \f$ \sum_{i=1}^n x^2 - x + 2 \f$
+    double x;
+};
+
+}
+
+
+int main() {
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
